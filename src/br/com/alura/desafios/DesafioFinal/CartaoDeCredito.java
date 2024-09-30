dbdf0d8c86761f490048cd3497fc339bbcbbd466
faximon <vitor.faximon@gmail.com>
2024-09-30 00:21:41 -0300

    Desafio Final

---
+++ src/br/com/alura/desafios/DesafioFinal/CartaoDeCredito.java
@@ -0,0 +1,37 @@
+package br.com.alura.desafios.DesafioFinal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoDeCredito {
+    private double limite;
+    private double saldo;
+    private List<Compra> compras;
+
+    public CartaoDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+    public boolean lancaCompra(Compra compra) {
+        if (saldo >= compra.getValor() && compra.getValor() > 0) {
+            saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
+}
